b25f14dfdd2ebf4ce1c1891e5bdd8f2564e95c46
Anderson <chltjdrhd777@naver.com>
2021-10-10 15:12:41 +0900

    style : learn how to navigate scroll section by using # and id

--- portfolio_website/main.js
+++ portfolio_website/main.js
@@ -1,1 +1,28 @@
 //JS
+const body = document.querySelector("body");
+const menueToggle = document.querySelector(".hamburder-menu");
+const hero = document.querySelector(".hero");
+const navList = document.querySelector(".nav-list");
+
+navList.addEventListener("click", function (e) {
+  let target = e.target;
+
+  if (target.classList[0] === "nav-link") {
+    body.classList.remove("is-open");
+  }
+});
+
+menueToggle.addEventListener("click", function () {
+  body.classList.toggle("is-open");
+  hero.classList.add("is-open");
+});
+
+window.addEventListener("click", function (e) {
+  let clicked = e.target;
+  console.dir(window);
+
+  if (clicked.matches(".is-open")) {
+    body.classList.remove("is-open");
+    hero.classList.remove("is-open");
+  }
+});
